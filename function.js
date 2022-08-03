6fcd5d4e401c72719b36acb4ebd47ee67b31fa29
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-03 23:17:25 +0600

    function

---
+++ function.js
@@ -0,0 +1,9 @@
+// function decleration
+function startFan() {
+    console.log('Stand up');
+    console.log('Walk towards the switch');
+    console.log('Press the switch');
+}
+
+// function call
+startFan();
