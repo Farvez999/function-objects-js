d747527d7ccd408eff87164eca623b002ef2cb11
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-04 10:23:08 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,19 @@
+const color = 'yellow';
+
+switch (color) {
+    case 'green':
+        console.log('your are green friend');
+        break;
+    case 'blue':
+        console.log('your are blue friend');
+        break;
+    case 'red':
+        console.log('your are red friend');
+        break;
+    case 'yellow':
+        console.log('your are yellow friend');
+        break;
+    case 'pink':
+        console.log('your are pink friend');
+        break;
+}
